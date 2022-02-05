ec421cc51c29424828ffa71daab3509b37af3480
rupeshbhagoria <bhagoriarupesh99@gmail.com>
2022-02-05 21:41:34 +0545

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "MeroPurse",
+    home: Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('MeroPurse'),
+      ),
+    ),
+  ));
+}
